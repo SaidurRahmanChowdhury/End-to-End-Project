55f9b47001258caf5370e5cc5659ea654b7328be
Saidur Rahman Chowdhury <saidur.rahman.chowdhury98@gmail.com>
2023-09-04 22:41:08 +0600

    Data ingestion is completed

--- source/mlproject/components/data_ingestion.py
+++ source/mlproject/components/data_ingestion.py
@@ -3,6 +3,8 @@ import sys
 from source.mlproject.exception import CustomException
 from source.mlproject.logger import logging
 import pandas as pd
+from source.mlproject.utils import read_sql_data
+from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 @dataclass
@@ -17,10 +19,22 @@ class DataIngestion:
         
     def initiate_data_ingestion(self):
         try:
-            
-            logging.info("Reading from Mysql Database")
+            df=read_sql_data()
+            logging.info("Reading completed from Mysql Database")
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            df.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            df.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            
+            logging.info('Data Ingestion is completed')
+            
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
         except Exception as e:
             raise CustomException(e,sys)
     
