b24fd380d3a8782720dd68eafb92cca91f245e75
Saidur Rahman Chowdhury <saidur.rahman.chowdhury98@gmail.com>
2023-09-24 14:32:51 +0600

    Model Trained

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from source.mlproject.logger import logging
 from source.mlproject.exception import CustomException
 from source.mlproject.components.data_ingestion import DataIngestion
 from source.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
+from source.mlproject.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 
 if __name__=="__main__":
@@ -12,7 +13,12 @@ if __name__=="__main__":
         data_ingestion=DataIngestion()
         train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_array,test_array,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        
+        # Model Training
+        
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_array,test_array))
         
     except Exception as e:
         logging.info("Custom Exception")
--- source/mlproject/components/data_ingestion.py
+++ source/mlproject/components/data_ingestion.py
@@ -19,7 +19,7 @@ class DataIngestion:
         
     def initiate_data_ingestion(self):
         try:
-            # df=read_sql_data()
+            #df=read_sql_data()
             df=pd.read_csv(os.path.join('notebook/data','raw.csv'))
             logging.info("Reading completed from Mysql Database")
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
